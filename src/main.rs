6352dde0f228d046f292ae0df3f5e3d34e5b8341
Winfried Baumann <frieder@holoride.com>
2023-09-04 09:59:40 +0200

    with padding

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,12 @@ fn main() {
 
 fn encode(file_path: PathBuf) {
     let file_content = fs::read(file_path).expect("Specified file not found");
-    let encoded = base64::engine::general_purpose::GeneralPurpose::encode(&general_purpose::STANDARD_NO_PAD, &file_content);
+    let encoded = base64::engine::general_purpose::GeneralPurpose::encode(&general_purpose::STANDARD, &file_content);
     println!("Encoded File:\n{}", encoded);
 }
 
 fn decode(text: String) {
-    let decoded = base64::engine::general_purpose::GeneralPurpose::decode(&general_purpose::STANDARD_NO_PAD, &text).unwrap();
+    let decoded = base64::engine::general_purpose::GeneralPurpose::decode(&general_purpose::STANDARD, &text).unwrap();
     match String::from_utf8(decoded) {
         Ok(v) => println!("Decoded Text:\n{}", v),
         Err(e) => panic!("Invalid UTF-8 sequence passed as argument: {}", e),
